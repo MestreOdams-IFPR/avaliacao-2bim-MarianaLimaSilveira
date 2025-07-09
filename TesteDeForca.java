6d1c614d22b4b90804a68bc076c47981a5f8bf56
MarianaLimaSilevira <maridasilveira9@gmail.com>
2025-07-09 10:42:51 -0300

    correcao, tentei arrumar

--- TesteDeForca.java
+++ TesteDeForca.java
@@ -12,46 +12,27 @@ public class TesteDeForca {
         return vetor;
     }
 
-    public static int[] quickSort(int[] vetor){
-        quick2(vetor,0,vetor.length-1);
+    public static int[] selectionSort(int[] vetor) {
+        for (int i = 0; i < vetor.length - 1; i++) {
+            int p = i;
+            
+            for (int j = i + 1; j < vetor.length; j++) {
+                if (vetor[j] < vetor[p]) {
+                    p = j;
+                }
+            }
+            
+            if (p != i) {
+                swap(vetor, i, p);
+            }
+        }
+ 
         return vetor;
     }
 
-    public static void quick2(int[] vetor, int inicio, int fim){
-        int meio, pivo, i, j;
-
-        if (inicio<fim) {
-            meio = (inicio + fim)/2;
-            pivo = vetor[meio];
-
-            swap(vetor,meio,fim);
-            i = inicio -1;
-            j = fim;
-
-            do{
-                do{
-                    i++;
-                }while(i<=j && vetor[i] < pivo);
-
-                do{
-                    j--;
-                }while(i<=j && vetor[j] < pivo);
-
-                if(i<=j){
-                    swap(vetor, i, j);
-                }
-            }while(i<j);
-
-            vetor[fim] = vetor [i];
-            vetor[i] = pivo;
-            quick2(vetor, inicio, i-1);
-            quick2(vetor, i+1, fim);
-        }
-
-    }
    
     public static int determinarForçaMinima(int[] pontuacoes, int vagas){
-        int[]ordenado = quickSort(pontuacoes);
+        int[]ordenado = selectionSort(pontuacoes);
         int forcaMinima = 0;
 
         for(int k = (pontuacoes.length-1) - vagas; k < ordenado.length;k++){
